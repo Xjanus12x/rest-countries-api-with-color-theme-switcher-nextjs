b84e6ddf40c8c42c56e125d88dfa34dab87b3119
xjanus12x <albertovillacarlos07@gmail.com>
2025-02-04 16:21:59 +0800

    fix: escape unescaped double quotes in DisplayCountries

--- src/components/DisplayCountries.tsx
+++ src/components/DisplayCountries.tsx
@@ -21,7 +21,8 @@ export function DisplayCountries({
     <section className="grid gap-10 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 md:gap-14 xl:gap-16">
       {countries?.length === 0 ? (
         <p className="text-center text-lg text-gray-500 col-span-full">
-          No countries found matching "{query}" {region && `in ${region}`}.
+          No countries found matching &quot;{query}&quot;
+          {region && `in ${region}`}.
           <br />
           Try refining your search or check if the region and country name are
           correct.
@@ -29,7 +30,7 @@ export function DisplayCountries({
       ) : (
         countries?.map((country) => (
           <Link
-           className="hover:-translate-y-2 transition-transform focus-visible:-translate-y-4 outline-offset-4"
+            className="hover:-translate-y-2 transition-transform focus-visible:-translate-y-4 outline-offset-4"
             href={`/country?name=${
               country.name.common || country.name.official
             }`}
